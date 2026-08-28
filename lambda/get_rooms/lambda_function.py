5ade6d5a72d4ef3e06471c53f3100b87591b5511
Your Name <natcha.kanch@dome.tu.ac.th>
2026-08-28 23:59:40 +0700

    chore: configure Python environment for Lambda

--- lambda/get_rooms/lambda_function.py
+++ lambda/get_rooms/lambda_function.py
@@ -1,39 +1,5 @@
 import json
-#mock data รอdatabase
+import os
+import pymysql
 
-def lambda_handler(event, context):
-    rooms = [
-        {
-            "room_id": "R001",
-            "room_name": "ห้องเรียน 1",
-            "room_type": "Classroom",
-            "capacity": 40,
-            "facilities": ["Projector", "Air Conditioner"],
-            "available_time": "08:00-18:00"
-        },
-        {
-            "room_id": "R002",
-            "room_name": "ห้องปฏิบัติการคอมพิวเตอร์",
-            "room_type": "Computer Laboratory",
-            "capacity": 50,
-            "facilities": ["Computer", "Projector", "Air Conditioner"],
-            "available_time": "08:00-18:00"
-        },
-        {
-            "room_id": "R003",
-            "room_name": "ห้องประชุม 1",
-            "room_type": "Meeting Room",
-            "capacity": 20,
-            "facilities": ["Projector", "Air Conditioner"],
-            "available_time": "08:00-18:00"
-        }
-    ]
 
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*"
-        },
-        "body": json.dumps(rooms, ensure_ascii=False)
-    }
